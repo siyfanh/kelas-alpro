231e2c985648e126072b5291681f33ca33a1b23a
siyfanh <siyfanh17@gmail.com>
2023-11-01 10:29:50 +0700

    file baru

--- Tugas06-Integral/integral.c
+++ Tugas06-Integral/integral.c
@@ -1,29 +1,35 @@
-#include<stdio.h>
+#include <stdio.h>
 
-// Fungsi yang akan diintegrasikan
-double f(double x) {
-    return 2*x + 3;
+double f(double x)
+{
+    return 2 * x + 3;
 }
 
-// Metode trapesium
-double trapezoidal(double a, double b, int n) {
-    double h = (b-a)/n;
+double trapezoidal(double a, double b, int n)
+{
+    double h = (b - a) / n;
     double s = f(a) + f(b);
 
     for (int i = 1; i < n; i++)
-        s += 2 * f(a + i*h);
+        s += 2 * f(a + i * h);
 
     return (h / 2) * s;
 }
 
-int main() {
-    double a = -1, b = 4; // batas integrasi
-    int n; // jumlah blok
-
+int main()
+{
+    double a = -1, b = 4;
+    int n;
+    
+    printf("Masukkan batas bawah: ");
+    scanf("%f", &a);
+    printf("Masukkan batas atas: ");
+    scanf("%f", &b);
     printf("Masukkan jumlah blok (N): ");
     scanf("%d", &n);
+    
 
     printf("Hasil integral: %.6lf\n", trapezoidal(a, b, n));
 
-    return 0;
+    return 0;
 }
