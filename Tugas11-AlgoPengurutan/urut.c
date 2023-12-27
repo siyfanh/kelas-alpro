c27bec2fe5db2cc8ebc47101a59c0a6acf5fe464
siyfanh <146912574+siyfanh@users.noreply.github.com>
2023-12-27 10:45:38 +0700

    tambah tugas11

---
+++ Tugas11-AlgoPengurutan/urut.c
@@ -0,0 +1,39 @@
+#include <stdio.h>
+
+#define MAX 5
+
+void TampilkanArray(int A[], int n)
+{
+    for (int j = 0; j < n; j ++)
+    {
+        printf("A[%d] = %d\n", j, A[j]);
+    }
+}
+
+int main()
+{
+    int A[MAX] = { 40, 4, 30, 8, 7};
+    int j, k;
+    int temp;
+
+    printf("Sebelum pengurutan :\n");
+    TampilkanArray(A, MAX);
+
+    for (j = 0; j < MAX-1; j++)
+    {
+        for (k = MAX-1; k >= (j+1); k--)
+        {
+            if (A[k] < A[k-1])
+            {
+                temp = A[k];
+                A[k] = A[k-1];
+                A[k-1] = temp;
+            }
+        }
+    }
+    printf("\n");
+
+    printf("Setelah pengurutan :\n");
+    TampilkanArray(A, MAX);
+    return 0;
+}
