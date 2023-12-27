453366cafb49f26fc028845600505f6077b949f1
siyfanh <146912574+siyfanh@users.noreply.github.com>
2023-12-27 09:46:18 +0700

    BinarySearch2.c

--- Tugas10-AlgoPencarian/BinarySearch2.c
+++ Tugas10-AlgoPencarian/BinarySearch2.c
@@ -1,5 +1,5 @@
 #include <stdio.h>
-
+#include <time.h>
 #define SIZE 100
 size_t binarySearch(const int b[], int searchKey, size_t low, size_t high);
 int main(){
@@ -14,7 +14,10 @@ int main(){
     int searchKey;
     scanf("%d", &searchKey);
 
+    clock_t begin = clock();
     size_t index = binarySearch(a, searchKey, LOW, HIGH);
+    clock_t end = clock();
+
     if (index != -1)
     {
         printf("Found value at index %d\n", index);
@@ -23,6 +26,8 @@ int main(){
     {
         puts("Value not found");
     }
+    double time_spent = (double)(end-begin)/CLOCKS_PER_SEC;
+    printf("Time spent : %f seconds\n", time_spent);
 }
 
 size_t binarySearch(const int b[], int searchKey, size_t low, size_t high){
